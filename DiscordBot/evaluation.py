62eb486bb2799c49b58913a09a7f71cb35b65380
Ayesha Khawaja <ayesha.khawaja01@gmail.com>
2024-05-29 01:21:37 -0700

    Finalized Google Perspective code, model results on hatemoji dataset

--- DiscordBot/evaluation.py
+++ DiscordBot/evaluation.py
@@ -1,33 +1,50 @@
 import pandas as pd
-import time
+import time, csv
 from perspective import check_hate_speech
 
 hatemoji_df = pd.read_csv('../data/hatemojicheck.csv')
+perspective_results = 'perspective_results.csv'
 
 def evaluate_perspective_model(data):
     results = {}
-    correct, true_pos, true_neg, false_pos, false_neg = 0, 0, 0, 0, 0
+    correct, true_pos, true_neg, false_pos, false_neg = [], [], [], [], []
     total = len(data)
+    errors = []
     for index, row in data.iterrows():
-        pred_hate_speech = check_hate_speech(row.text)
-        if pred_hate_speech == row.label_gold:
-            correct += 1
-        if pred_hate_speech and row.label_gold:  # we predicted hate speech and it is
-            true_pos += 1
-        if not pred_hate_speech and not row.label_gold:  # we predicted not hate speech and it's not
-            true_neg += 1
-        if pred_hate_speech and not row.label_gold:  # we predicted hate speech but it's not
-            false_pos += 1
-        if not pred_hate_speech and row.label_gold:  # we predicted not hate speech but it is
-            false_neg += 1
-        if index % 60 == 0:  # avoid perspective quota limits
+        try:
+            pred_hate_speech = check_hate_speech(row.text)
+            if pred_hate_speech == row.label_gold:
+                correct.append(row.text)
+            if pred_hate_speech and row.label_gold:  # we predicted hate speech and it is
+                true_pos.append(row.text)
+            if not pred_hate_speech and not row.label_gold:  # we predicted not hate speech and it's not
+                true_neg.append(row.text)
+            if pred_hate_speech and not row.label_gold:  # we predicted hate speech but it's not
+                false_pos.append(row.text)
+            if not pred_hate_speech and row.label_gold:  # we predicted not hate speech but it is
+                false_neg.append(row.text)
+        except:
+            errors.append(row.text)
+            total -= 1
+        if index % 50 == 0:  # avoid perspective quota limits (60 requests per minute)
+            print(index)
             time.sleep(60)
-    results["accuracy"] = correct / total
-    results["true_positives"] = true_pos
-    results["true_negatives"] = true_neg
-    results["false_positives"] = false_pos
-    results["false_negatives"] = false_neg
-    results["precision"] = true_pos / (true_pos + false_pos)
-    results["recall"] = true_pos / (true_pos + false_neg)
-    results["prevalence"] = false_neg / total
+    results["accuracy"] = len(correct) / total
+    results["precision"] = len(true_pos) / (len(true_pos) + len(false_pos))
+    results["recall"] = len(true_pos) / (len(true_pos) + len(false_neg))
+    results["prevalence"] = len(false_neg) / total
+    results["true_positives"] = len(true_pos), true_pos
+    results["true_negatives"] = len(true_neg), true_neg
+    results["false_positives"] = len(false_pos), false_pos
+    results["false_negatives"] = len(false_neg), false_neg
+    results["errors"] = errors
     return results
+
+# random_rows = hatemoji_df.sample(50)  # for testing
+data = evaluate_perspective_model(hatemoji_df)
+
+# save results to csv
+with open(perspective_results, 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=data.keys())
+    writer.writeheader()
+    writer.writerow(data)
--- DiscordBot/perspective.py
+++ DiscordBot/perspective.py
@@ -24,30 +24,28 @@ def check_hate_speech(message):
     analyze_request = {
         'comment': {'text': message},
         'requestedAttributes': {
-            'TOXICITY': {}, # adding "scoreThreshold": 0.5 will only return scores above 0.5
+            'TOXICITY': {},  # adding "scoreThreshold": 0.5 will only return scores above 0.5
             'SEVERE_TOXICITY': {},
             'IDENTITY_ATTACK': {},
             "INSULT": {},
-            "PROFANITY": {},
             'THREAT': {},
         }  
     }
-    response = client.comments().analyze(body=analyze_request).execute() # send request to API
+    response = client.comments().analyze(body=analyze_request).execute()  # send request to API
 
     # extract scores from response
     toxicity_score = response['attributeScores']['TOXICITY']['summaryScore']['value']
     severe_toxicity_score = response['attributeScores']['SEVERE_TOXICITY']['summaryScore']['value']
     identity_attack_score = response['attributeScores']['IDENTITY_ATTACK']['summaryScore']['value']
     insult_score = response['attributeScores']['INSULT']['summaryScore']['value']
-    profanity_score = response['attributeScores']['PROFANITY']['summaryScore']['value']
     threat_score = response['attributeScores']['THREAT']['summaryScore']['value']
-    scores = [toxicity_score, severe_toxicity_score, identity_attack_score, insult_score, profanity_score, threat_score]
+    scores = [toxicity_score, severe_toxicity_score, identity_attack_score, insult_score, threat_score]
     hate_speech_scores = [severe_toxicity_score, identity_attack_score, threat_score]
 
     # based on the scores, determine if the message is hate speech
-    if any(score > 0.9 for score in scores):  # if one score is really high, flag it
+    if any(score > 0.8 for score in scores):  # if one score is really high, flag it
         return True
-    if any(score > 0.7 for score in hate_speech_scores):  # if specific hate speech indicators are high, flag it
+    if any(score > 0.3 for score in hate_speech_scores):  # if specific hate speech indicators are high, flag it
         return True
 
     # so far, these seem like bad indicators of hate speech
@@ -57,7 +55,7 @@ def check_hate_speech(message):
     #     return True
     
     # # maybe do a weighted average?
-    # weights = [0.1, 0.25, 0.25, 0.1, 0.05, 0.25]
+    # weights = [0.1, 0.25, 0.25, 0.1, 0.25]
     # weighted_scores = [score * weight for score, weight in zip(scores, weights)]
     # weighted_avg = sum(weighted_scores)
     # if weighted_avg > 0.75:
@@ -65,4 +63,4 @@ def check_hate_speech(message):
 
     return False
 
-print(check_hate_speech("You are terrible and I hope you die."))
+print(check_hate_speech("I don't talk to f🌟ggots."))
